7ef31805f5f9dba71e034479bcb45772f2f59854
Robert MacRae <github@therealm18.com>
2025-09-10 18:49:36 -0300

    added new FlashMemoryItems

--- src/main/java/com/therealm18studios/oc2rc/common/item/Items.java
+++ src/main/java/com/therealm18studios/oc2rc/common/item/Items.java
@@ -1,10 +1,8 @@
 package com.therealm18studios.oc2rc.common.item;
 
 import com.therealm18studios.oc2rc.OC2RC;
-import li.cil.oc2.common.item.MemoryItem;
-import li.cil.oc2.common.item.HardDriveItem;
-import li.cil.oc2.common.item.CPUItem;
-import li.cil.oc2.common.item.FloppyItem;
+import li.cil.oc2.common.Constants;
+import li.cil.oc2.common.item.*;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -52,6 +50,15 @@ public final class Items {
         new FloppyItem(2949120));
     public static final RegistryObject<FloppyItem> FLOPPY_MODERN = register("floppy_modern", () ->
         new FloppyItem(5898240));
+
+    public static final RegistryObject<FlashMemoryItem> FLASH_MEMORY_SMALL = register("flash_memory_small", () ->
+        new FlashMemoryItem(16 * Constants.MEGABYTE));
+    public static final RegistryObject<FlashMemoryItem> FLASH_MEMORY_MEDIUM = register("flash_memory_medium", () ->
+        new FlashMemoryItem(20 * Constants.MEGABYTE));
+    public static final RegistryObject<FlashMemoryItem> FLASH_MEMORY_LARGE = register("flash_memory_large", () ->
+        new FlashMemoryItem(24 * Constants.MEGABYTE));
+    public static final RegistryObject<FlashMemoryItem> FLASH_MEMORY_EXTRA_LARGE = register("flash_memory_extra_large", () ->
+        new FlashMemoryItem(28 * Constants.MEGABYTE));
 
     ///////////////////////////////////////////////////////////////////
 
